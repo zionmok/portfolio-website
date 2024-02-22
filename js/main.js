1079a1ebec313134ad938b66e4c0ef2517a5572c
zion <mok.xinyiz@gmail.com>
2024-02-22 19:53:43 +0800

    added scroll effect for navbar

--- js/main.js
+++ js/main.js
@@ -38,3 +38,16 @@
     window.scroll({top: 0, left: 0, behavior: 'smooth'});
   }
 
+
+  // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "1px 60px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "30px 60px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
